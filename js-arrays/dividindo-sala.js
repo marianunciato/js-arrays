7ebd9d7da93bb59a82b354b446be6afc24c295d1
Maria Eduarda Anunciato <122390168+marianunciato@users.noreply.github.com>
2023-03-21 17:41:48 -0300

    Add files via upload

---
+++ js-arrays/dividindo-sala.js
@@ -0,0 +1,28 @@
+const alunos = [
+    "João",
+    "Juliana",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Alie",
+    "Fabiana",
+    "André",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isabela",
+    "Vinicius",
+    "Renan",
+    "Renata",
+    "Daisy",
+    "Camilo",
+];
+
+const sala1 = alunos.slice(0, 10);
+const sala2 = alunos.slice(10);
+
+console.log(sala1);
+console.log(sala2);
---
+++ js-arrays/map-nome.js
@@ -0,0 +1,7 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'Bia silva'];
+
+const nomesPadronizados = nomes.map((nome) => {
+    return nome.toUpperCase();
+});
+
+console.log(nomesPadronizados);
